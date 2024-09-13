51ac5ab33a2ba3f05baab405ca8dabfc8bf4171d
amretasrerengarajan <rengarajanthiruven.a@northeastern.edu>
2024-09-13 12:09:54 -0400

    Day and night modes

--- amretasre-portfolio/src/components/Navbar/Navbar.jsx
+++ amretasre-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
+// import {} from "../../../assets"
 
 export const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -10,6 +11,35 @@ export const Navbar = () => {
     const handleMenuItemClick = (menuItem) => {
         setSelectedItem(menuItem);
     };
+
+    const toggleDarkMode = () => {
+        const root = document.documentElement;
+        const currentTheme = root.getAttribute('data-theme');
+
+        const themeImage = document.getElementById('themeImage'); // Get the image element
+
+        if (currentTheme === 'dark') {
+            root.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light'); 
+            themeImage.src = getImageUrl("icons/happy_moon.png"); 
+        } else {
+            root.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark'); 
+            themeImage.src = getImageUrl("icons/sun.png"); 
+        }
+    };
+
+    // Immediately set theme based on saved preference when the page loads
+    document.addEventListener('DOMContentLoaded', () => {
+        const savedTheme = localStorage.getItem('theme') || 'light';
+        const root = document.documentElement;
+        const themeImage = document.getElementById('themeImage');
+
+        root.setAttribute('data-theme', savedTheme);
+        themeImage.src = savedTheme === 'dark' ? getImageUrl("icons/sun.png") : getImageUrl("icons/happy_moon.png");
+    });
+
+
 
     const handleOutsideClick = (event) => {
         if (navbarRef.current && !navbarRef.current.contains(event.target)) {
@@ -35,7 +65,14 @@ export const Navbar = () => {
                 <li className={selectedItem === 'experience' ? styles.selectedMenuItem : ''}><a href="#experience" onClick={() => handleMenuItemClick('experience')}>Experience</a></li>
                 <li className={selectedItem === 'projects' ? styles.selectedMenuItem : ''}><a href="#projects" onClick={() => handleMenuItemClick('projects')}>Projects</a></li>
                 <li className={selectedItem === 'contact' ? styles.selectedMenuItem : ''}><a href="#contact" onClick={() => handleMenuItemClick('contact')}>Contact</a></li>
+                <li>
+                    <span className={styles.modeButtonContainer} onClick={toggleDarkMode} id="themeButton">
+                        <img id="themeImage" className={styles.modeButton} src={getImageUrl("icons/happy_moon.png")} alt="Theme" />
+                    </span>
+                </li>
             </ul>
+
+
         </div>
     </nav>;
 }
